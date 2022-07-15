eba9080a1a648b22c34193afca08ed9d7afb5571
Athirasajeev <31205577+Athirasajeev@users.noreply.github.com>
2022-07-15 14:46:53 +0530

    Add files via upload

---
+++ OOPS_Concepts/src/firstp/Addition.java
@@ -0,0 +1,14 @@
+package firstp;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=90;
+		int b=20;
+		int c=a+b;
+		System.out.println("Sum="+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OOPS_Concepts/src/module-info.java
@@ -0,0 +1,2 @@
+module OOPS_Concepts {
+}
